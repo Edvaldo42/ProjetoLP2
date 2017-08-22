f8bc7e5a9a4d1975b1fac6c7b93074ed49a23442
andre <andre.matos@ccc.ufcg.edu.br>
2017-08-22 13:35:58 -0300

    Tinham faltados os docs de CRUDUsuario :thinking:

--- src/usuario/CRUDUsuario.java
+++ src/usuario/CRUDUsuario.java
@@ -8,7 +8,16 @@ import exceptionsComplementares.UsuarioCadastradoException;
 import exceptionsUsuario.UsuarioInvalidoException;
 
 public class CRUDUsuario {
-
+	
+	/**
+	 * Cadastra um usuario no sistema
+	 * 
+	 * @param nome O nome do usuario a ser cadastrado
+	 * @param telefone O telefone do usuario a ser cadastrado
+	 * @param email O email do usuario a ser cadastrado
+	 * @throws UsuarioCadastradoException Caso o usuario ja esteja cadastrado
+	 * @throws StringInvalidaException Caso alguma String seja invalida
+	 */
 	public static void cadastraUsuario(String nome, String telefone, String email, Set<Usuario> usuarios)
 			throws UsuarioCadastradoException, StringInvalidaException {
 
@@ -18,11 +27,30 @@ public class CRUDUsuario {
 		}
 	}
 
+	/**
+	 * Remove o usuario do sistema
+	 * 
+	 * @param nome O nome do usuario
+	 * @param telefone O telefone do usuario 
+	 * @param usuarios O conjunto onde esse usuario será adicionado
+	 * @throws UsuarioInvalidoException Caso o usuario seja invalido
+	 */
 	public static void removerUsuario(String nome, String telefone, Set<Usuario> usuarios) throws UsuarioInvalidoException {
 		Usuario user = buscaUsuario(nome, telefone, usuarios);
 		usuarios.remove(user);
 	}
 
+	/**
+	 * Atualiza uma informacao, passada no atributo, de um usuario
+	 * 
+	 * @param nome O nome do usuario a ter uma informacao atualizada
+	 * @param telefone O telefone do usuario a ter uma informacao atualizada
+	 * @param atributo O atributo que devera ser atualizado
+	 * @param valor A nova informacao a ser atualizada
+	 * @param usuarios O conjunto onde esse usuario será atualizado
+	 * @throws UsuarioInvalidoException Caso o usuario seja invalido
+	 * @throws AtributoInvalidoException Caso o atributo seja invalido
+	 */
 	public static void atualizarUsuario(String nome, String telefone, String atributo, String valor, Set<Usuario> usuarios) throws UsuarioInvalidoException, AtributoInvalidoException {
 		
 		Usuario user = buscaUsuario(nome, telefone, usuarios);
@@ -41,6 +69,16 @@ public class CRUDUsuario {
 		}
 	}
 
+	/**
+	 * Retorna uma informacao, passada no atributo, do usuario
+	 * 
+	 * @param nome O nome do usuario de que se saber alguma informacao
+	 * @param telefone O telefone do usuario de que se saber alguma informacao
+	 * @param atributo A informacao que se quer saber sobre um usuario
+	 * @param usuarios O conjunto onde esse usuario será buscado
+	 * @return A informacao desejada de um usuario
+	 * @throws StringInvalidaException Caso uma string seja invalida
+	 */
 	public static String getInfoUsuario(String nome, String telefone, String atributo, Set<Usuario> usuarios) throws StringInvalidaException {
 		Usuario user = buscaUsuario(nome, telefone, usuarios);
 
@@ -68,6 +106,15 @@ public class CRUDUsuario {
 		return info;
 	}
 
+	/**
+	 * Retorna um usuario especifico do sistema
+	 * 
+	 * @param nome O nome do usuario que se busca
+	 * @param telefone O telefone do usuario que se busca
+ 	 * @param usuarios O conjunto onde esse usuario será buscado
+	 * @return O usuario buscado
+	 * @throws UsuarioInvalidoException 
+	 */
 	public static Usuario buscaUsuario(String nome, String telefone, Set<Usuario> usuarios) throws UsuarioInvalidoException {
 		for (Usuario usuario: usuarios) {
 			if (usuario.getNome().equals(nome) && usuario.getTelefone().equals(telefone)) {
